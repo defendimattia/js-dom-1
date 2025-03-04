f4a51558cba17da3a8144e7ebba0e0bd0f2688a8
defendimattia <defendi.mattia92@gmail.com>
2025-03-04 15:35:50 +0100

    add: bonus

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@ const lampImg = document.getElementById("lamp-img");
 
 switchBtn.addEventListener("click",
     function () {
-        lampImg.src = "img/yellow_lamp.png"
+        if (lampImg.src.includes("white_lamp.png")) {
+            lampImg.src = "img/yellow_lamp.png"
+            lampImg.classList.add("fixed-img")
+            switchBtn.innerHTML = "Spegni"
+        }
+        else {
+            lampImg.src = "img/white_lamp.png";
+            lampImg.classList.remove("fixed-img");
+            switchBtn.innerHTML = "Accendi";
+        }
     }
 )
