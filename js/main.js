31a3cfb9f2340c087eefda3b365ce4c7e81b19ed
defendimattia <defendi.mattia92@gmail.com>
2025-03-04 14:47:43 +0100

    add: img and button working, need to be fixed

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const switchBtn = document.getElementById("switch-btn");
+const lampImg = document.getElementById("lamp-img");
+
+switchBtn.addEventListener("click",
+    function () {
+        lampImg.src = "img/yellow_lamp.png"
+    }
+)
